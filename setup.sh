dfc9698dbd0a84894d9f749d1234a54e9f770a84
Justin Nichols <ninetyonenichols@gmail.com>
2020-06-13 08:00:04 -0700

    Added ability to make file-system for CSC337 project.

---
+++ setup.sh
@@ -0,0 +1,17 @@
+# creates the basic files / directories needed for a CSC337 (Web Development)
+# 	 project
+
+# Making directories
+mkdir public_html
+cd public_html
+mkdir img
+mkdir css
+
+# Making files
+touch README.htlm
+touch index.html
+
+# Git 
+git init
+git add .
+git commit -m "Created placeholder files / directories."
